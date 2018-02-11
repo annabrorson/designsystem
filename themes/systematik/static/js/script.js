c2eb19b1067ca17e9dd2e5f34d7d490f8fa5414a
annabrorson <31202699+annabrorson@users.noreply.github.com>
2018-02-11 18:24:34 +0100

    Add files via upload

---
+++ themes/systematik/static/js/script.js
@@ -0,0 +1,65 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+//
+//---------------- LIGHTBOX (modalvindue til billede) ----------------
+
+$('a').nivoLightbox(); 
+
+
+
+//---------------- /*Ajax KONTAKTFORMULAR*/----------------
+
+$("#kontaktmig").submit(function(event) {
+           // cancels the form submission
+           event.preventDefault();
+           submitForm();
+       });
+
+       function submitForm() {
+           // Initiate Variables With Form Content
+           var navn = $("#navn").val();
+           var email = $("#email").val();
+           var henvendelse = $("#henvendelse").val();
+           var besked = $("#besked").val();
+
+           $.ajax({
+               type: "POST",
+               url: "formhandler.php",
+               data: "navn=" + navn + "&email=" + email + "&henvendelse=" + henvendelse + "&besked=" + besked,
+               success: function(text) {
+                   if (text == "success") {
+                       formSuccess();
+                   } else{
+                       formFailure(text);
+                   }
+               }
+           });
+       }
+
+       function formSuccess() {
+           $("#formfeedback").removeClass("hidden");
+           $("#kontaktmig").addClass("hidden");
+           $("#formfailure").addClass("hidden");
+       }
+       function formFailure(text) {
+           $("#formfailure").removeClass("hidden");
+           $("#formfailure").html(text);
+       }
+
+
+//
+//------------ TWEENMAX ------------
+
+window.addEventListener("load", altErLoadet);
+
+function altErLoadet(){
+
+TweenMax.staggerFrom(".column_and_name_container p", 1, {opacity:"0"},-.1);
+TweenMax.staggerFrom(".column_and_name_container svg", 1, {width:"0"},.1);
+}
+
